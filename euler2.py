c0d95424abf7849829c1c00ca1393a5c2219fdf5
Jack Martin <JM273606@gmail.com>
2015-01-18 22:35:12 -0500

    updates

---
+++ euler2.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python
+
+
+#make a fibonacci sequence
+#make the sequence stop at 4,000,000. 
+n = [1]
+x = []
+while n < 4000000:
+	n.[:-2] + n.[:-1]
+		return 
+	if number % 2 == 0: 
+		n.append(x) 
+print n
+
+
+#add the even numbers to a count 
+
+#sum(even numbers) 
+r = range(1,1000)
+count = 0
+for number in r:
+	if number % 3 == 0 or number % 5 == 0:
+		count += number
+print count
